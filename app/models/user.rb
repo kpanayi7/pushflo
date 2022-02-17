61667ef2463834f40ca0a4effe70b082f310b195
Kyri Panayi <kpanayi7@gmail.com>
2022-02-17 21:27:49 +0000

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :user_name, :email, presence: true
+  validates :email, :user_name, confirmation: true, uniqueness: true
+  validates :user_name, length: { maximum: 20 }
 end
