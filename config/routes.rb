f519b2fc94d1613c25b00234f689f0cddf750154
Nelson Yepes <nelsond.yepes@gmail.com>
2022-02-19 15:32:13 +0000

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   root to: 'pages#home'
+  get 'users/:id', to: 'users#show', as: 'profile'
+  get 'users/:id/edit', to: 'users#edit', as: 'update_user'
+  patch 'users/:id', to: 'users#update', as: 'edit_user'
+  put 'users/:id', to: 'users#update'
+
+  resources :workflows, only: [:new, :create, :update, :edit]
+  resources :workflows, only: [:index, :show]
+  get 'workflows/:id', to: 'workflows#index_by_program', as: :program
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
