b9e2a2f9ef457a15928131209d3ee9631608652e
Nelson Yepes <nelsond.yepes@gmail.com>
2022-02-19 15:10:08 +0000

    schema updated

--- db/migrate/20220219143410_add_last_name_to_users.rb
+++ db/migrate/20220219143410_add_last_name_to_users.rb
@@ -1,7 +1,5 @@
 class AddLastNameToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :last_name, :string
-    add_column :users, :user_name, :string
-    add_column :users, :first_name, :string
   end
 end
