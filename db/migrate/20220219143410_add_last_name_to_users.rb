340a2975268974679fc24c6c5554a51d932011a6
Dave Sellick <dave@sidgrove.com>
2022-02-19 15:02:42 +0000

    db updates

--- db/migrate/20220219143410_add_last_name_to_users.rb
+++ db/migrate/20220219143410_add_last_name_to_users.rb
@@ -1,5 +1,7 @@
 class AddLastNameToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :last_name, :string
+    add_column :users, :user_name, :string
+    add_column :users, :first_name, :string
   end
 end
