8358bbae8fe6961603f460a6f119b7695ce3bc42
Kyri Panayi <kpanayi7@gmail.com>
2022-03-03 20:08:36 +0000

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Favourite.destroy_all
 Workflow.destroy_all
 User.destroy_all
 
-User.create!(first_name:"Dave", last_name:"Sellick", user_name:"dave", email:"dave@pushflo.com", password:"1234567", admin: true)
+pushflo = User.create!(first_name:"Dave", last_name:"Sellick", user_name:"dave", email:"dave@pushflo.com", password:"1234567", admin: true)
 puts "created user"
 
 
@@ -28,23 +28,23 @@ puts "created user"
 end
 
 
-  Workflow.create!(
+  dave = Workflow.create!(
     title: "Loom Shortcuts",
     description: "Loom Video Shortcuts Pack",
     walkthrough: "This pack will take you through some of the best shortcuts to speed up the creation of videos with Loom. From automating record on/off to turning on your lights and throwing in some confetti",
     program: "Loom",
     loom_url:"https://www.loom.com/share/6cceaf0c4d884f8a9a1b1995c1444efb",
-    user_id: 1
+    user: pushflo
   )
   puts "created workflow"
 
-  Workflow.create!(
+  kyri = Workflow.create!(
     title: "Headings in Outlook",
     description: "Bash out the next level headings on the fly",
     walkthrough: "Getting your headings out for the lads - this will supercharge your headings in Outlook",
     program: "Outlook",
     loom_url:"https://www.loom.com/share/3faebe6e53fc47cc8a2822101d0bb7c8",
-    user_id: 1
+    user: pushflo
   )
   puts "created workflow"
 
@@ -55,7 +55,7 @@ Workflow.create!(
     walkthrough: "This is how you bosh borders on the fly",
     program: "Excel",
     loom_url:"https://www.loom.com/share/eaf6d9b477bc4c478493bcf484f3a191",
-    user_id: 1
+    user: pushflo
 )
   puts "created workflow"
 
@@ -65,7 +65,7 @@ Workflow.create!(
     walkthrough: "This is how you bosh Notion shortcuts on the fly",
     program: "Notion",
     loom_url:"https://www.loom.com/share/afa9ffb550e24441867577d5fa033c52",
-    user_id: 1
+    user: pushflo
 )
   puts "created workflow"
 
@@ -75,31 +75,31 @@ Workflow.create!(
     walkthrough: "This is how you bosh Gmail textstings on the fly",
     program: "Gmail",
     loom_url:"https://www.loom.com/share/fedc02d5b2214ee0b20c34aa5f3eace8",
-    user_id: 1
+    user: pushflo
 )
   puts "created workflow"
 
 
 
 
-10.times do
-  Favourite.create!(
-    user_id: rand(1..10),
-    workflow_id: rand(1..5)
-  )
-  puts "created favourite"
-end
+# 10.times do
+#   Favourite.create!(
+#     user_id: rand(1..10),
+#     workflow_id: rand(1..5)
+#   )
+#   puts "created favourite"
+# end
 
 
 
   Favourite.create!(
-    user_id: 1,
-    workflow_id:2
+    user: pushflo,
+    workflow: dave
   )
   puts "created favourite"
 
    Favourite.create!(
-    user_id: 1,
-    workflow_id:3
+    user: pushflo,
+    workflow: kyri
   )
   puts "created favourite"
