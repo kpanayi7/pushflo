1c419f2d69ac1791f68f5ae3f369aacf911bb5a7
Kyri Panayi <kpanayi7@gmail.com>
2022-03-08 19:32:23 +0000

    fix environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,11 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack');
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
