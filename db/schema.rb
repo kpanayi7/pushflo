3201ba6b3e010efa863ea23c7f299ac159efb8e1
Dave Sellick <dave@sidgrove.com>
2022-03-26 18:28:24 +0000

    uploads sidebar and user sidebar updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_104516) do
+ActiveRecord::Schema.define(version: 2022_03_26_164059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_104516) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "loom_url"
+    t.boolean "approved", default: false
     t.index ["user_id"], name: "index_workflows_on_user_id"
   end
 
